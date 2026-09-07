86d30968caf8d287891a1aa4c1cd4e30702ddee0
めいそ <k024g0101@g.neec.ac.jp>
2026-09-08 01:04:34 +0900

    利益追加

---
+++ NobleEngine/src/application/GameObject/UI/Benefit/Benefit.cpp
@@ -0,0 +1,65 @@
+#include "Benefit.h"
+#include"../Numbers/Numbers.h"
+Benefit::Benefit()
+{
+    
+    Numbers::Load();
+
+    numbers_.resize(maxDigit);
+    for (int i = 0; i < maxDigit; ++i) {
+        numbers_[i] = std::make_unique<Numbers>();
+    }
+}
+
+Benefit::~Benefit()
+{
+}
+
+void Benefit::Initialize()
+{
+    for (int i = 0; i < maxDigit; ++i) {
+        numbers_[i]->Initialize(0, { i * 0.5f,0.0f,0.0f });
+    }
+}
+
+void Benefit::Update(const int32_t cameraID)
+{
+
+    if (isUpdateBenefit_) {
+
+        int32_t tempBenefit = benefit_;
+
+        for (int digit = maxDigit-1; digit >= 0; digit--) {
+            int digitNum = std::powf(10, digit);
+            int num = tempBenefit / digitNum;
+            numbers_[maxDigit-1]->SetModelId(num);
+            tempBenefit %= digitNum;
+        }
+    }
+
+    for (int i = 0; i < maxDigit; ++i) {
+        numbers_[i]->Update(cameraID);
+    }
+}
+
+void Benefit::Draw()
+{
+    for (int i = 0; i < maxDigit; ++i) {
+        numbers_[i]->Draw();
+    }
+}
+
+void Benefit::DrawImGui()
+{
+
+    ImGui::Begin("UI");
+
+    static int32_t benefit = 0;
+    ImGui::SliderInt("benefit", &benefit, 0, 100000);
+    
+    if (ImGui::Button("UpdateBenefit")) {
+        SetBenefit(benefit);
+    }
+
+    ImGui::End();
+}
---
+++ NobleEngine/src/application/GameObject/UI/Numbers/Numbers.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<Game.h>
+
+class Numbers
+{
+private:
+    // モデルID
+    static std::vector<int32_t> modelIDs_;
+    //グラス
+    std::unique_ptr<RenderObject> obj_ = nullptr;
+
+    EulerTransforms transform_;
+    Matrix4x4 worldMatrix_;
+    Vector4 color_;
+ 
+public:
+    static void Load();
+    Numbers();
+    ~Numbers();
+    void SetStencil(const DepthStencilID id);
+    void Initialize(const uint32_t number, const Vector3& position, const Vector3& rotation = {0.0f,3.14f,0.0f},const Vector3& scale = {1.0f,1.0f,1.0f});
+    void Update(const int32_t cameraID);
+    void SetModelId(const uint32_t number);
+    void Draw();
+    void DrawImGui();
+};
+
