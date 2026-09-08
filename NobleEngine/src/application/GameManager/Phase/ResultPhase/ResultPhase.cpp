999fd1ec7f62e8272f8a5e567f4302f48de135d2
Kashiwagi-Haruka <k024g0020@g.neec.ac.jp>
2026-09-09 03:05:08 +0900

    リザルトシーン作成

---
+++ NobleEngine/src/application/GameManager/Phase/ResultPhase/ResultPhase.cpp
@@ -0,0 +1,16 @@
+#include "ResultPhase.h"
+
+ResultPhase::ResultPhase(){}
+ResultPhase::~ResultPhase() {}
+void ResultPhase::Initialize() {
+	// 結果画面の初期化処理をここに記述
+}
+void ResultPhase::Update() {
+	// 結果画面の更新処理をここに記述
+}
+void ResultPhase::Draw() {
+	// 結果画面の描画処理をここに記述
+}
+void ResultPhase::DrawImGui() {
+	// ImGuiを使用した結果画面の描画処理をここに記述
+}
